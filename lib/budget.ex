be656af3d9974efe74a121bd69e3e8ac6933264a
sixtead <sixtead@gmail.com>
2017-08-22 16:23:19 +0300

    Add transactions.csv file

--- lib/budget.ex
+++ lib/budget.ex
@@ -1,17 +1,5 @@
 defmodule Budget do
-  @moduledoc """
-  Documentation for Budget.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Budget.hello
-      :world
-
-  """
+  
   def hello do
     :world
   end
